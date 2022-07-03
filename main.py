8a1e374211a23dd752259b11d3624cd5942ad3b8
programista10kpln <programista10kpln@proton.me>
2022-07-03 13:55:35 +0200

    added sfd scrapping

--- main.py
+++ main.py
@@ -11,33 +11,44 @@ def get_protein_from_kfd():
     numbers = [i.find('a', attrs={'rel': 'nofollow'}).text.strip() for i in page_list if
                i.find('a', attrs={'rel': 'nofollow'})]
 
-    products = {}
+    products_name_price = {}
 
     for number in range(1, int(numbers[-1]) + 1):
         current_url = url + f'&page={number}'
         current_site = requests.get(current_url)
         current_soup = BeautifulSoup(current_site.content, 'html.parser')
-        descriptions = current_soup.findAll('div', class_='product-description')
-        for description in descriptions:
-            # name = description.find('h2', class_='h3 product-title')
-            name = description.find('a')
-            price = description.find('span', class_='price')
-            products[name.text.strip()] = price.text.strip()
+        product_tiles = current_soup.findAll('div', class_='product-description')
+        for tile in product_tiles:
+            name = tile.find('a')
+            price = tile.find('span', class_='price')
+            products_name_price[name.text.strip()] = price.text.strip()
 
-    # for i in products:
-    #     yield i.text
+    return products_name_price
 
-    return products
 
-    # numbers = []
-    # for i in page_list:
-    #     number = i.find('a', attrs={'rel': 'nofollow'})
-    #     if number:
-    #         numbers.append(number.text.strip())
-    # return numbers
+def get_protein_from_sfd():
+    url = 'https://sklep.sfd.pl/aspx/produkty.aspx?katid=119'
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    page_list_block = soup.find('span', class_='pagination')
+    numeric_strings = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    numbers = [i for i in list(page_list_block.text.strip()) if i in numeric_strings]
+
+    products_name_price = {}
+
+    for number in range(1, int(numbers[-1]) + 1):
+        current_url = url + f'&page={number}'
+        current_site = requests.get(current_url)
+        current_soup = BeautifulSoup(current_site.content, 'html.parser')
+        product_tiles = current_soup.findAll('div', class_='product-tile__info-box')
+        for tile in product_tiles:
+            name = tile.find('h3')
+            price = tile.find('span', class_='cena')
+            name_formatted = ' '.join(name.text.split())
+            products_name_price[name_formatted] = price.text.strip()
+
+    return products_name_price
 
 
 print(get_protein_from_kfd())
-
-# for i in get_protein_from_kfd():
-#     print(i)
+print(get_protein_from_sfd())
