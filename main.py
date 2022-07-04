46bf64690c6ff18c9613f9fb677b11c79cfe51ab
programista10kpln <programista10kpln@proton.me>
2022-07-04 09:41:54 +0200

    changed data structure, added myprotein, working on bodyhouse

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def get_protein_from_kfd():
     numbers = [i.find('a', attrs={'rel': 'nofollow'}).text.strip() for i in page_list if
                i.find('a', attrs={'rel': 'nofollow'})]
 
-    products_name_price = {}
+    products = []
 
     for number in range(1, int(numbers[-1]) + 1):
         current_url = url + f'&page={number}'
@@ -21,9 +21,15 @@ def get_protein_from_kfd():
         for tile in product_tiles:
             name = tile.find('a')
             price = tile.find('span', class_='price')
-            products_name_price[name.text.strip()] = price.text.strip()
+            link = tile.find('a')
 
-    return products_name_price
+            products.append({
+                'name': name.text.strip(),
+                'price': price.text.strip().replace('\xa0', ' '),
+                'link': link['href']
+            })
+
+    return products
 
 
 def get_protein_from_sfd():
@@ -31,10 +37,10 @@ def get_protein_from_sfd():
     site = requests.get(url)
     soup = BeautifulSoup(site.content, 'html.parser')
     page_list_block = soup.find('span', class_='pagination')
-    numeric_strings = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    numbers = [i for i in list(page_list_block.text.strip()) if i in numeric_strings]
+    page_list = page_list_block.findAll()
+    numbers = [i.text.strip() for i in page_list if i.text.isdigit()]
 
-    products_name_price = {}
+    products = []
 
     for number in range(1, int(numbers[-1]) + 1):
         current_url = url + f'&page={number}'
@@ -43,12 +49,70 @@ def get_protein_from_sfd():
         product_tiles = current_soup.findAll('div', class_='product-tile__info-box')
         for tile in product_tiles:
             name = tile.find('h3')
-            price = tile.find('span', class_='cena')
             name_formatted = ' '.join(name.text.split())
-            products_name_price[name_formatted] = price.text.strip()
+            price = tile.find('span', class_='cena')
+            link = tile.find('a')
 
-    return products_name_price
+            products.append({
+                'name': name_formatted,
+                'price': price.text.strip(),
+                'link': f'https:{link["href"]}'
+            })
+
+    return products
 
 
-print(get_protein_from_kfd())
-print(get_protein_from_sfd())
+def get_protein_from_my_protein():
+    url = 'https://www.myprotein.pl/nutrition/protein.list'
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    page_list_block = soup.find('ul', class_='responsivePageSelectors')
+    page_list = page_list_block.findAll('li')
+    numbers = [i.text.strip() for i in page_list if i.text.strip()]
+
+    products = []
+
+    for number in range(1, int(numbers[-1]) + 1):
+        current_url = url + f'?pageNumber={number}'
+        current_site = requests.get(current_url)
+        current_soup = BeautifulSoup(current_site.content, 'html.parser')
+        product_tiles = current_soup.findAll('li', class_='productListProducts_product')
+        for tile in product_tiles:
+            name = tile.find('div', class_='athenaProductBlock_title')
+            price = tile.find('div', class_='athenaProductBlock_priceBlock')
+            link = tile.find('a')
+
+            products.append({
+                'name': name.text.strip(),
+                'price': price.text.strip().replace('\n', ' ').split('ł')[0] + 'ł',
+                'link': f'https://www.myprotein.pl{link["href"]}'
+            })
+
+    return products
+
+
+def get_protein_from_body_house():  # doesnt work
+    url = 'https://bodyhouse.pl/pol_m_Suplementy_Odzywki-bialkowe-307.html#1000'
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, 'html.parser')
+
+    products = []
+
+    product_tiles = soup.findAll('div', class_='product_wrapper')
+    # for tile in product_tiles:
+    #     name = tile.find('div', class_='athenaProductBlock_title')
+    #     price = tile.find('div', class_='athenaProductBlock_priceBlock')
+    #     link = tile.find('a')
+    #
+    #     products.append({
+    #         'name': name.text.strip(),
+    #         'price': price.text.strip().replace('\n', ' ').split('ł')[0] + 'ł',
+    #         'link': f'https://www.myprotein.pl{link["href"]}'
+    #     })
+
+    return soup
+
+# print(get_protein_from_kfd())
+# print(get_protein_from_sfd())
+# print(get_protein_from_my_protein())
+# print(get_protein_from_body_house())
