330126931d7ac23ca32d0e7fa760d9dc28be5945
programista10kpln <programista10kpln@proton.me>
2022-07-07 12:54:43 +0200

    code cleanup in sfd

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def get_protein_from_kfd():
     url = "https://sklep.kfd.pl/bialko-c-50.html"
 
     querystring = {"q": "Waga-kg-0.7-16/Dostępność-Dostępne", "page": "1"}
-
     payload = ""
     headers = {}
+
     site = requests.request("GET", url, data=payload, headers=headers, params=querystring)
     soup = BeautifulSoup(site.content, 'html.parser')
     page_list_block = soup.find('ul', class_='page-list clearfix text-sm-center')
@@ -29,13 +29,13 @@ def get_protein_from_kfd():
         product_tiles = current_soup.findAll('div', class_='product-description')
         for tile in product_tiles:
             name = tile.find('a').text.strip()
-            price = tile.find('span', class_='price').text.strip().replace('\xa0', ' ')
-            price_numeric = float(re.findall(r'\d*,*\d+', price, re.IGNORECASE)[0].replace(',', '.'))
+            price = tile.find('span', class_='price').text.strip().replace('\xa0', ' ').replace(',', '.')
             try:
                 quantity = re.findall(r'\d*,*\d+ *k* *g+', name, re.IGNORECASE)[0]
             except IndexError:
                 quantity = None
             if quantity:
+                price_numeric = float(re.findall(r'\d*,*\d+', price, re.IGNORECASE)[0])
                 quantity_numeric = float(re.findall(r'\d*,*\d+', quantity, re.IGNORECASE)[0].replace(',', '.'))
                 if 'k' in quantity.lower():
                     quantity_numeric = int(quantity_numeric * 1000)
@@ -59,36 +59,55 @@ def get_protein_from_kfd():
 
 
 def get_protein_from_sfd():
-    url = 'https://sklep.sfd.pl/aspx/produkty.aspx?katid=119'
-    site = requests.get(url)
+    url = "https://sklep.sfd.pl/aspx/produkty.aspx"
+
+    querystring = {"katid": "119", "page": "1"}
+    payload = ""
+    headers = {}
+
+    site = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
     soup = BeautifulSoup(site.content, 'html.parser')
     page_list_block = soup.find('span', class_='pagination')
     page_list = page_list_block.findAll()
-    numbers = [i.text.strip() for i in page_list if i.text.isdigit()]
+    numbers = [i.text.strip() for i in page_list if i.text.strip().isdigit()]
 
     products = []
 
-    for number in range(1, int(numbers[-1]) + 1):
-        current_url = url + f'&page={number}'
-        current_site = requests.get(current_url)
+    for number in numbers:
+        querystring = {"katid": "119", "page": number}
+        payload = ""
+        headers = {}
+
+        current_site = requests.request("GET", url, data=payload, headers=headers, params=querystring)
         current_soup = BeautifulSoup(current_site.content, 'html.parser')
         product_tiles = current_soup.findAll('div', class_='product-tile__info-box')
         for tile in product_tiles:
-            name = tile.find('h3')
-            name_formatted = ' '.join(name.text.split())
-            quantity = re.findall(r'\d+ *k* *g+', name_formatted, re.IGNORECASE)
-            price = tile.find('span', class_='cena').text
-            price_numeric = float('.'.join(re.findall(r'\d+', price, re.IGNORECASE)))
-            quantity_numeric = int(re.findall(r'\d+', name_formatted, re.IGNORECASE)[0])
-            price_for_100g = round((price_numeric / quantity_numeric) * 100, 2)
-            link = tile.find('a')
+            name = ' '.join(tile.find('h3').text.split())
+            price = tile.find('span', class_='cena').text.strip().replace(',', '.')
+            try:
+                quantity = re.findall(r'\d*,*\d+ *k* *g+', name, re.IGNORECASE)[0]
+            except IndexError:
+                quantity = None
+            if quantity:
+                price_numeric = float(re.findall(r'\d*,*\d+', price, re.IGNORECASE)[0])
+                quantity_numeric = float(re.findall(r'\d*,*\d+', quantity, re.IGNORECASE)[0].replace(',', '.'))
+                if 'k' in quantity.lower():
+                    quantity_numeric = int(quantity_numeric * 1000)
+                    quantity = f'{quantity_numeric} g'
+                else:
+                    quantity_numeric = int(quantity_numeric)
+                price_for_100g = f'{round((price_numeric / quantity_numeric) * 100, 2)} zł/100g'
+            else:
+                price_for_100g = None
+            link = f'https:{tile.find("a")["href"]}'
 
             products.append({
-                'name': name_formatted,
-                'price': price.strip(),
-                'quantity': quantity[0],
+                'name': name,
+                'price': price,
+                'quantity': quantity,
                 'price for 100g': price_for_100g,
-                'link': f'https:{link["href"]}'
+                'link': link
             })
 
     return products
@@ -149,9 +168,7 @@ def get_protein_from_body_house():
     return products
 
 
-print(get_protein_from_kfd())
-# proteins = get_protein_from_sfd()
-# tanio = sorted(proteins, key=lambda item: item['price for 100g'])
-# print(tanio)
+# print(get_protein_from_kfd())
+print(get_protein_from_sfd())
 # print(get_protein_from_my_protein())
 # print(get_protein_from_body_house())
